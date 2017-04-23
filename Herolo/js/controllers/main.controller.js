f5d818a8a101758c728d8e97b970a76b82d20791
natibeks <natibeks@gmail.com>
2017-04-23 11:18:33 +0300

    Update main.controller.js

--- Herolo/js/controllers/main.controller.js
+++ Herolo/js/controllers/main.controller.js
@@ -1,8 +1,9 @@
-﻿var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute']);
 
 app.controller('MainController', function ($rootScope) {
     $rootScope.dataLoading = true;
     $rootScope.loadingError = "";
+    // show server error alert if getting reject from getting data promise
     $rootScope.$on("$routeChangeError", function (event, current, previous, rejectionMsg) {
         if (typeof (previous) == "undefined") {
             $rootScope.loadingError = rejectionMsg;
