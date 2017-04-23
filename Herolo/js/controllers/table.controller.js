21ca437f4152dcf3134835f0e7afa7aa1cb298b0
natibeks <natibeks@gmail.com>
2017-04-23 11:18:51 +0300

    Update table.controller.js

--- Herolo/js/controllers/table.controller.js
+++ Herolo/js/controllers/table.controller.js
@@ -1,4 +1,4 @@
-﻿app.controller('TableController', function ($scope, $rootScope, $http, $filter, books) {
+app.controller('TableController', function ($scope, $rootScope, $http, $filter, books) {
 
     $scope.books = books;
     $rootScope.dataLoading = false;
@@ -30,12 +30,14 @@
         $scope.editMode = 0;
     }
 
+    // show/hide edit/delete btns on row hover
     $scope.hoverInBookRow = function (book) {
         book.ShowBtn = true;
     };
     $scope.hoverOutBookRow = function (book) {
         book.ShowBtn = false;
     };
+
     $scope.getRowIndex = function (arr, id) {
         var index;
         angular.forEach(arr, function (o, i) {
@@ -50,7 +52,7 @@
         { Title: "Date", Width: "200px" },
         { Title: "Title", Width: "auto" }];
 
-    // pagination
+    // pagination code
     $scope.pager = {
         curr: 0,
         size: 5,
